c1fb1e646ad38a935e3995a51dca2768af91c56a
Scalsol <scalsol@163.com>
2017-12-28 13:38:22 +0800

    finish softmax

--- assignment1/cs231n/classifiers/softmax.py
+++ assignment1/cs231n/classifiers/softmax.py
@@ -30,6 +30,20 @@ def softmax_loss_naive(W, X, y, reg):
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
+  num_classes = W.shape[1]
+  num_train = X.shape[0]
+  for i in xrange(num_train):
+    scores = X[i].dot(W)
+    exp_scores = np.exp(scores)
+    pred_prob = exp_scores / np.sum(exp_scores)
+    correct_class_prob = pred_prob[y[i]]
+    loss -= np.log(correct_class_prob)
+    grad_scores = pred_prob
+    grad_scores[y[i]] -= 1
+    dW += np.reshape(X[i], (3073, 1)).dot(np.reshape(grad_scores, (1, 10)))
+
+  loss /= num_train
+  dW /= num_train
   pass
   #############################################################################
   #                          END OF YOUR CODE                                 #
@@ -54,6 +68,20 @@ def softmax_loss_vectorized(W, X, y, reg):
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
+  num_classes = W.shape[1]
+  num_train = X.shape[0]
+
+  scores = X.dot(W)
+  exp_scores = np.exp(scores)
+  pred_prob = exp_scores / np.reshape(np.sum(exp_scores, axis=1), (num_train, 1))
+  correct_class_prob = pred_prob[np.arange(num_train), y]
+  loss -= np.sum(np.log(correct_class_prob))
+  grad_scores = pred_prob
+  grad_scores[np.arange(num_train), y] -= 1
+  dW += X.T.dot(grad_scores)
+
+  loss /= num_train
+  dW /= num_train
   pass
   #############################################################################
   #                          END OF YOUR CODE                                 #
